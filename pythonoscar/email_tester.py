edfefdecdd5cdbe75e7dd2aa1589d9c1840ed76b
Erik Scarlatescu <saerik2001@gmail.com>
2024-05-24 12:45:39 -0400

    Recent change

--- pythonoscar/email_tester.py
+++ pythonoscar/email_tester.py
@@ -8,50 +8,54 @@ import time
 def send_email(recipients, subject="Lorem ipsum dolor sit amet", body="consectetur adipiscing elit"):
     print("Sending email about: " + subject)
     gmail_user = 'saerik2001@gmail.com'
-    gmail_password = 'Rikoitza2001'
+    gmail_password = 'ydtrjsxuecyevnxd'
 
-    sent_from = gmail_user
+    sent_from = 'saerik2001@gmail.com'
     to = recipients
 
-    email_text = """\
-    From: %s
-    To: %s
-    Subject: %s
+    message = """From: Course Notifier <saerik2001@gmail.com>
+To:  <%s>
+Subject: %s
 
-    %s
-    """ % (sent_from, ", ".join(to), subject, body)
+%s
+    """ % ('', subject, body)
 
     try:
         smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         smtp_server.ehlo()
         smtp_server.login(gmail_user, gmail_password)
-        smtp_server.sendmail(sent_from, to, email_text)
+        smtp_server.sendmail(sent_from, to, message)
         smtp_server.close()
-        print ("Email sent successfully!")
     except Exception as ex:
         print ("Something went wrong….",ex)
 
-crns = ["32618", "27177", "27663"]
+send_email(['saerik2001@gmail.com'], 'Starting up OSCAR server', 'The server is started.')
+
+emails = ['saerik2001@gmail.com']
+
+crns = ['85373', '94299', '88359', '92643']
+addresses = [0, 0, 0, 0]
 courses = [ Course(crn) for crn in crns ]
 spots_open = [ 0 for x in crns ]
 
-REFRESH_INTERVAL = 20
+REFRESH_INTERVAL = 5
 
 while True:
     for i in range(len(courses)):
         course = courses[i]
         #print(course.name)
-        num_spots = course.get_registration_info("202202")['vacant']
+        num_spots = course.get_registration_info("202308")['vacant']
+        print(str(course.name) + ": " + str(course.get_registration_info("202308")))
 
         if spots_open[i] == 0 and num_spots > 0:
             print("Spot open in " + course.name)
             spots_open[i] = num_spots
 
-            send_email("saerik2001@gmail.com", "Class", "Spot open in " + course.name)
+            send_email(emails[addresses[i]], course.name + " is open.", "Spot open in " + course.name)
         if spots_open[i] > 0 and num_spots == 0:
             print("Spot closed in " + course.name)
             spots_open[i] = num_spots
 
-            send_email("saerik2001@gmail.com", "Class", "Spot closed in " + course.name)
+            send_email(emails[addresses[i]], course.name + " is closed.", "Spot closed in " + course.name)
     
     time.sleep(REFRESH_INTERVAL)
